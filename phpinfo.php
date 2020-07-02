9101fe55af8a3029f47becf4cf0ea9640e0a3a5b
unknown <sohamj123@gmail.com>
2020-07-02 13:42:24 -0700

    changed php

--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,14 @@
 <?php
-print("hello world");
-phpinfo();
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
