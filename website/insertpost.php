76028457327cbbc8619d55c2a82af402d95a8144
unknown <sohamj123@gmail.com>
2020-07-06 12:40:02 -0400

    insert

--- website/insertpost.php
+++ website/insertpost.php
@@ -2,16 +2,27 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-
+$dbname = "nbatakes";
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+date_default_timezone_set('America/New_York');
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-var sql ="INSERT INTO post (title, take) VALUES ('" + $_POST["title"] + "')"
 
-echo "Connected successfully";
-echo ;
+$sql = sprintf('INSERT INTO post (title, take,dt) VALUES ("%s", "%s", "%s")', $_POST["title"] , $_POST["take"] , date("Y-m-d H:i:s") );
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully. " . date_default_timezone_get() ;
+  header("Location: take.html");
+  exit();
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
 ?>
